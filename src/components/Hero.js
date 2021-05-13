ecb7723e0308bb81dc8aba46cc33a3c7c7e2f7fc
wizdo-johan <soldierheart@hotmail.com>
2021-05-13 11:50:06 +0200

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Link } from "gatsby"
+import socialLinks from "../constants/social_links"
+import heroImg from "../assets/images/hero.svg"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Hero = () => {
+  return <header className="hero">
+    <section className="section-center hero-center">
+      <article className="hero-info">
+        <div>
+          <div className="underline" />
+          <h1>I'm Johan</h1>
+          <h4>Freelance web and mobile UI/UX designer</h4>
+          <Link to="/contact" className="btn">
+            Contact me
+          </Link>
+          <div className="social-links">
+            {socialLinks.map(link => {
+              return <a href={link.url} key={link.id} className="social-link">
+                {link.icon}
+              </a>
+            })}
+          </div>
+        </div>
+      </article>
+      {/*<StaticImage 
+        src="../assets/images/hero.svg" 
+        alt="portfolio" 
+        className="hero-img" 
+        placeholder="blurred" 
+      />*/}
+      <img src={heroImg} alt="portfolio" className="hero-img-svg" /> 
+    </section>
+  </header>
+}
+
+export default Hero
